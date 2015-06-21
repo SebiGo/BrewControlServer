46b7ad0c977e87004d579391e52702ba0ad718c8
Sebastian Goodrick <sebastian@goodrick.ch>
2015-06-21 21:08:45 +0200

    test

--- src/test/java/ch/goodrick/brewcontrol/BrewControlTest.java
+++ src/test/java/ch/goodrick/brewcontrol/BrewControlTest.java
@@ -1,12 +1,36 @@
 package ch.goodrick.brewcontrol;
 
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.apache.cxf.service.factory.ServiceConstructionException;
 import org.junit.Test;
+import org.junit.internal.runners.statements.Fail;
 
 public class BrewControlTest {
 
-	@Test
-	public void test() {
-		// there is no useful test scenario for this main class.
+	@Test(expected = ServiceConstructionException.class)
+	public void testMainGPIO() throws Exception {
+		String[] argv = new String[1];
+		argv[0] = "gpio";
+		BrewControl.main(argv);
+		fail();
+	}
+
+	@Test(expected = UnsatisfiedLinkError.class)
+	public void testMainPIFACE() throws Exception {
+		String[] argv = new String[1];
+		argv[0] = "piface";
+		BrewControl.main(argv);
+		fail();
+	}
+
+	@Test(expected = UnknownParameterException.class)
+	public void testMain() throws Exception {
+		String[] argv = new String[1];
+		argv[0] = "bla";
+		BrewControl.main(argv);
 	}
 
 }
