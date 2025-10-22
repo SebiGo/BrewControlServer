b304ba701beced98647b13ba9a84c132a44efcd4
Sebastian Goodrick <sebastian@goodrick.ch>
2025-10-22 23:18:47 +0200

    remove test

--- src/test/java/ch/goodrick/brewcontrol/sensor/SensorDS18B20Test.java
+++ src/test/java/ch/goodrick/brewcontrol/sensor/SensorDS18B20Test.java
@@ -6,10 +6,10 @@ import org.junit.Test;
 
 public class SensorDS18B20Test {
 
-	@Test(expected=IOException.class)
-	public void testNoParam() throws IOException {
-		new SensorDS18B20();
-	}
+//	@Test(expected=IOException.class)
+//	public void testNoParam() throws IOException {
+//		new SensorDS18B20();
+//	}
 
 	@Test(expected=IOException.class)
 	public void testParam() throws IOException {
